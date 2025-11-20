7a95df28f6ac20e99311b1ae913cf8cc6fb586a5
baggr <baggrischa@icloud.com>
2025-11-20 13:51:06 +0400

    удален лишний импорт

--- app/bot.py
+++ app/bot.py
@@ -1,10 +1,8 @@
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from fastapi import FastAPI
 
 from .config import load_settings
 
 bot = Bot(token=load_settings().bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
-app = FastAPI()
